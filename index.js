67f899ab362c545b5c6dc7f4898929edbddb63a7
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-08-22 00:16:32 +0530

    Crud With File System

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const fs = require("fs");
 const path = require("path");
-const dirName = path.join(__dirname, "files");
-// console.log(dirName);
-for (i = 0; i < 5; ++i) {
-  fs.writeFileSync(dirName + `/hello${i}.txt`, "A simple Test File");
-}
-fs.readdir(dirName, (err, files) => {
-  files.forEach((item) => {
-    console.log("File name is ", item);
-  });
-});
+const dirPath = path.join(__dirname, "crud");
+const filePath = `${dirPath}/apple.txt`;
+// fs.writeFileSync(filePath, "This is a simple text file");
+// fs.readFile(filePath, "utf8", (err, item) => {
+//   console.log(item);
+// });
+// Updation
+// fs.appendFile(filePath, "and file name is apple.txt", (err) => {
+//   if (!err) console.log("File is Updated");
+// });
+// Rename
+// fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+//   if (!err) {
+//     console.log("File name is Updated");
+//   }
+// });
+// Deletion
+fs.unlinkSync(`${dirPath}/fruit.txt`);
