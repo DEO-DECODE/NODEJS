0723fead270e35d49ddb431237e60bd2959aa415
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-08-22 01:08:44 +0530

    Basic Routing

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-const fs = require("fs");
-const path = require("path");
-const dirPath = path.join(__dirname, "crud");
-const filePath = `${dirPath}/apple.txt`;
-// fs.writeFileSync(filePath, "This is a simple text file");
-// fs.readFile(filePath, "utf8", (err, item) => {
-//   console.log(item);
-// });
-// Updation
-// fs.appendFile(filePath, "and file name is apple.txt", (err) => {
-//   if (!err) console.log("File is Updated");
-// });
-// Rename
-// fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
-//   if (!err) {
-//     console.log("File name is Updated");
-//   }
-// });
-// Deletion
-fs.unlinkSync(`${dirPath}/fruit.txt`);
+const express = require("express");
+const app = express();
+
+app.get("", (req, res) => {
+  res.send("Hello This is Home Page");
+});
+app.get("/about", (req, res) => {
+  res.send("Hello, This is About Page");
+});
+app.get("/help", (req, res) => {
+  res.send("Hello , This is Help Page");
+});
+app.listen(5000);
