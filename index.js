fe3cfc64d61701ea0cd0917c029a465083177d35
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-08-22 01:19:11 +0530

    Render html in node js

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Hello This is Home Page");
+app.get("", (req, resp) => {
+  resp.send(`
+    <h1>Welcome to Home page</h1>
+    <a href="/about">Go to About page</a>
+    `);
 });
-app.get("/about", (req, res) => {
-  res.send("Hello, This is About Page");
+
+app.get("/about", (req, resp) => {
+  resp.send(`
+    <input type="text" placeholder="user name" value="${req.query.name}"  />
+    <button>Click</button>
+    <a href="/">Go to Home page</a>
+    `);
 });
-app.get("/help", (req, res) => {
-  res.send("Hello , This is Help Page");
+
+app.get("/help", (req, resp) => {
+  resp.send([
+    {
+      name: "Peter",
+      email: "peter@test.com",
+    },
+    {
+      name: "Bruce",
+      email: "bruce@test.com",
+    },
+  ]);
 });
+
 app.listen(5000);
