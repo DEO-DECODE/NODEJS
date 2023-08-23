42ce2e8654dd3105af72dffaf48719201cf25f5e
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-08-24 01:38:30 +0530

    Error not fixed for connection with mongodb

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
-const express = require("express");
-const app = express();
-const route = express.Router();
+const { MongoClient } = require("mongodb");
+const url = "mongodb://localhost:27017";
+const databaseName = "e-comm";
+const client = new MongoClient(url);
 
-const reqFilter = (req, resp, next) => {
-  if (!req.query.age) {
-    resp.send("Please provide your age");
-  } else if (req.query.age < 18) {
-    resp.send("You are under aged");
-  } else {
-    next();
+async function getData() {
+  try {
+    let result = await client.connect();
+    db = result.db(databaseName);
+    collection = db.collection("products");
+    let data = await collection.find({}).toArray();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
   }
-};
+}
 
-route.use(reqFilter);
-// app.use(reqFilter);
-
-app.get("/", (res, resp) => {
-  resp.send("Welcome to Home page");
-});
-
-app.get("/users", (res, resp) => {
-  resp.send("Welcome to Users page");
-});
-
-route.get("/about", (res, resp) => {
-  resp.send("Welcome to About page");
-});
-route.get("/contact", (res, resp) => {
-  resp.send("Welcome to contact page");
-});
-
-app.use("/", route);
-
-app.listen(5000);
+getData();
