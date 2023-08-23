abe72ba1f3255b25945bb12f3ff27b97388ab575
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-08-23 20:16:05 +0530

    Route Level Middleware

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const route = express.Router();
+
 const reqFilter = (req, resp, next) => {
-    if (!req.query.age) {
-        resp.send("Please provide your age")
-    }
-    else if (req.query.age<18) {
-        resp.send("You are under aged")
-    }
-    else {
-        next();
-    }
-}
+  if (!req.query.age) {
+    resp.send("Please provide your age");
+  } else if (req.query.age < 18) {
+    resp.send("You are under aged");
+  } else {
+    next();
+  }
+};
 
-app.use(reqFilter);
+route.use(reqFilter);
+// app.use(reqFilter);
 
-app.get('/', (res, resp) => {
-    resp.send('Welcome to Home page')
+app.get("/", (res, resp) => {
+  resp.send("Welcome to Home page");
 });
 
-app.get('/users', (res, resp) => {
-    resp.send('Welcome to Users page')
+app.get("/users", (res, resp) => {
+  resp.send("Welcome to Users page");
 });
-app.listen(5000)
+
+route.get("/about", (res, resp) => {
+  resp.send("Welcome to About page");
+});
+route.get("/contact", (res, resp) => {
+  resp.send("Welcome to contact page");
+});
+
+app.use("/", route);
+
+app.listen(5000);
