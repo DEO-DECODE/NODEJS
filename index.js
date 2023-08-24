9a27b08311f18b8c58c451eed4cf6372dbbcfd8a
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-08-25 02:37:31 +0530

    Connect MongoDB with Node

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const { MongoClient } = require("mongodb");
-const url = "mongodb://localhost:27017";
-const databaseName = "e-comm";
+const url = "mongodb://127.0.0.1/";
+const database = "e-comm";
 const client = new MongoClient(url);
 
 async function getData() {
-  try {
-    let result = await client.connect();
-    db = result.db(databaseName);
-    collection = db.collection("products");
-    let data = await collection.find({}).toArray();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
+  let result = await client.connect();
+  db = result.db(database);
+  collection = db.collection("products");
+  let data = await collection.find({}).toArray();
+  console.log(data);
 }
 
 getData();
