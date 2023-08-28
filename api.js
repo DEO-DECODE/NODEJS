1230d0701c64e55f6b058ceed2ba32d7d4529538
Deo Raj <d.jrajsingh81@gmail.com>
2023-08-28 23:03:25 +0530

    post API

--- api.js
+++ api.js
@@ -1,9 +1,21 @@
-const express = require("express");
 const dbConnect = require("./mongodb");
+const express = require("express");
 const app = express();
-app.get("/", async (req, resp) => {
+
+app.use(express.json());
+app.get("/", async (res, resp) => {
   let data = await dbConnect();
   data = await data.find().toArray();
   resp.send(data);
 });
+
+app.post("/", async (req, resp) => {
+  let data = await dbConnect();
+  let result = await data.insertOne(req.body);
+  if(result.acknowledged){
+    console.log("Data Inserted");
+  }
+  resp.send(result);
+});
+
 app.listen(5000);
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const dbConnect= require('./mongodb');
+const dbConnect = require("./mongodb");
 // dbConnect().then((resp) => {
 //   resp
 //     .find({ name: "poco c3" })
