0c6afcaaf40f0c482b7056c0ff76cf94b55fb0c0
Deo Raj <d.jrajsingh81@gmail.com>
2023-08-28 14:54:13 +0530

    Using Another function for establishing Connection

--- index.js
+++ index.js
@@ -3,21 +3,15 @@ const url = "mongodb://127.0.0.1/";
 const database = "e-comm";
 const client = new MongoClient(url);
 
-// async function getData() {
-//   let result = await client.connect();
-//   db = result.db(database);
-//   collection = db.collection("products");
-//   let data = await collection.find({}).toArray();
-//   console.log(data);
-// }
-// $ git config --global user.name "Deo Raj"
-// $ git config --global user.email d.jrajsingh81@gmail.com
-// Getting a Specific result
-async function getData() {
+async function dbConnect() {
   let result = await client.connect();
   db = result.db(database);
-  collection = db.collection("products");
-  let data = await collection.find({name: 'poco c3'}).toArray();
-  console.log(data);
+  return db.collection("products");
+  // let data = await collection.find({name: 'poco c3'}).toArray();
+  // console.log(data);
 }
-getData();
+dbConnect().then((resp)=>{
+  resp.find({name: 'poco c3'}).toArray().then((data)=>{
+    console.warn(data);
+  })
+})
