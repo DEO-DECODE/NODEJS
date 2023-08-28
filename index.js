521537cd062f6c2ffe3ad32e5427993088266026
Deo Raj <d.jrajsingh81@gmail.com>
2023-08-28 15:01:00 +0530

    Using Separate file for dbConnect

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const { MongoClient } = require("mongodb");
-const url = "mongodb://127.0.0.1/";
-const database = "e-comm";
-const client = new MongoClient(url);
-
-async function dbConnect() {
-  let result = await client.connect();
-  db = result.db(database);
-  return db.collection("products");
-  // let data = await collection.find({name: 'poco c3'}).toArray();
-  // console.log(data);
-}
-dbConnect().then((resp)=>{
-  resp.find({name: 'poco c3'}).toArray().then((data)=>{
-    console.warn(data);
-  })
-})
+const dbConnect= require('./mongodb');
+// dbConnect().then((resp) => {
+//   resp
+//     .find({ name: "poco c3" })
+//     .toArray()
+//     .then((data) => {
+//       console.warn(data);
+//     });
+// });
+const main = async () => {
+  let data = await dbConnect();
+  data = await data.find({}).toArray();
+  console.log(data);
+};
+main();
