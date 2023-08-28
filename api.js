fc062bd4f60037e49cdf7596e50ca11799e6918b
Deo Raj <d.jrajsingh81@gmail.com>
2023-08-28 23:25:16 +0530

    Delete API

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const dbConnect = require("./mongodb");
 const express = require("express");
 const app = express();
-
+const mongodb = require("mongodb");
 app.use(express.json());
 app.get("/", async (res, resp) => {
   let data = await dbConnect();
@@ -12,18 +12,23 @@ app.get("/", async (res, resp) => {
 app.post("/", async (req, resp) => {
   let data = await dbConnect();
   let result = await data.insertOne(req.body);
-  if(result.acknowledged){
+  if (result.acknowledged) {
     console.log("Data Inserted");
   }
   resp.send(result);
 });
 
-app.put("/:name", async(req, resp)=>{
-    const data= await dbConnect();
-    let result =data.updateOne(
-        {name: req.params.name},
-        {$set : req.body}
-    )
-    resp.send(result.acknowledged);
-})
+app.put("/:name", async (req, resp) => {
+  const data = await dbConnect();
+  let result = data.updateOne({ name: req.params.name }, { $set: req.body });
+  resp.send(result.acknowledged);
+});
+app.delete("/:id", async (req, resp) => {
+  console.log(req.params.id);
+  const data = await dbConnect();
+  const result = await data.deleteOne({
+    _id: new mongodb.ObjectId(req.params.id),
+  });
+  resp.send(result);
+});
 app.listen(5000);
