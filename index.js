01c5a68f1c09014af2a8b96295925655fe2f331e
Deo Raj <d.jrajsingh81@gmail.com>
2023-08-30 02:19:09 +0530

    Crud Operation with Node JS and Mongoose

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const mongoose = require("mongoose");
-const main = async () => {
-  await mongoose.connect("mongodb://127.0.0.1/e-comm");
-  const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
+
+mongoose.connect("mongodb://127.0.0.1/e-comm");
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  brand: String,
+  category: String,
+});
+
+const saveInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  let data = new Product({
+    name: "max 100",
+    price: 200,
+    brand: "maxx",
+    category: "Mobile",
   });
-  const productsModel = mongoose.model("products", productSchema);
-  let data = new productsModel({ name: "m 10", price: 1000 });
-  let result = await data.save();
+  const result = await data.save();
   console.log(result);
 };
-main();
+
+const updateInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  let data = await Product.updateOne(
+    { name: "max 6" },
+    {
+      $set: { price: 550, name: "max pro 6" },
+    }
+  );
+  console.log(data);
+};
+
+const deleteInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  let data = await Product.deleteOne({ name: "max 100" });
+  console.log(data);
+};
+const findInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  let data = await Product.find({ name: "max pro 611" });
+  console.log(data);
+};
+
+findInDB();
